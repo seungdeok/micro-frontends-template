d8e182f8532e8c478f5953681771699113adee37
seungdeok <akk9132@naver.com>
2024-07-01 23:46:48 +0900

    feat: add host app

---
+++ run-time-integration-js/vite-vue-react/host/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import federation from "@originjs/vite-plugin-federation";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    federation({
+      name: "host-app",
+      remotes: {},
+    }),
+  ],
+});
